6c80f64a5c5d56d033f6289591f0cab1a5a78a08
anatalphonset <anatalphonset@gmail.com>
2025-03-05 12:09:09 +0530

    layout 2 completed

--- brokart/products/urls.py
+++ brokart/products/urls.py
@@ -3,9 +3,10 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static 
 from django.conf import settings 
-from .import views
+from . import views
 
 urlpatterns = [
-    path('',views.index)
+    path('',views.index,name='index'),
+    path("product_list",views.list_products,name='list_product')
 ]
 urlpatterns +=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
