fc5d3360016a516303e77ed0383a23decef3def7
anatalphonset <anatalphonset@gmail.com>
2025-03-17 12:03:26 +0530

    paginator completed

--- brokart/products/views.py
+++ brokart/products/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from . models import Product
+from django.core.paginator import Paginator
+
 
 # Create your views here.
 
@@ -6,7 +9,14 @@ def index(request):
     return render(request,'index.html')
 
 def list_products(request):
-    return render(request,'products.html')
+    page=1
+    if request.GET:
+        page=request.GET.get('page',1)
+    products=Product.objects.all()
+    product_paginator=Paginator(products,4)
+    products=product_paginator.get_page(page)
+    context={'products':products}
+    return render(request,'products.html',context)
 
 
 def detail_product(request):
